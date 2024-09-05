8f19fa6572ea3f1206aa5e1be08a6f019ce39aae
Mourn5367 <qwerrrt6@gmail.com>
2024-09-05 21:41:06 +0900

    메뉴용으로 출력할것 하나 숫자로 입력받을 딕셔너리용 하나 문자로 입력받아서 매칭비교할 하나

---
+++ Dictionary_VM/Question.py
@@ -0,0 +1,24 @@
+class Question:
+    def __init__(self):
+        self.Ques = ["1. 추가 금액 넣기", "2. 메뉴 다시 고르기", "3. 그만 두기"]
+        self.QuesDict = self.CreateDict() # { 1 : "추가 금액 넣기"...
+        self.QuesNoneNumDict = self.CreateList() # ["추가금액넣기"...
+    def CreateDict(self):
+        QuestionDict = {}
+
+        for item in self.Ques:
+            dict_Key, dict_Value = int(item.split(".")[0]), item.split(".")[1].strip()
+            QuestionDict[dict_Key] = dict_Value
+        return QuestionDict
+
+    def CreateNoneNumDict(self):
+        QuestionNoneNumList = {}
+        # key와 Value 둘다 얻기 위해서는 딕셔너리.items()를 해줘야하고
+        # key만 얻기 위해서는 딕셔너리만 써도된다.
+        for key,value in self.QuesDict.items():
+            # strip 이건 양쪽 끝만 공백을 제거하고
+            # replace 이건 전부다 제거 할 수도 있고 3번째 인수에는 횟수를 지정할 수도 있다.
+            value = value.replace(" ", "")
+            QuestionNoneNumList[value] = value
+        return QuestionNoneNumList
+
