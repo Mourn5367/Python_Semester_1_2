e01cac9d5372dd5ba094eaf22532385f57cfa248
Mourn5367 <qwerrrt6@gmail.com>
2024-09-08 23:46:55 +0900

    안쓰는 필드 일단 주석처리

--- Dictionary_VM/Question.py
+++ Dictionary_VM/Question.py
@@ -2,7 +2,7 @@ class Question:
     def __init__(self):
         self.list = ["1. 추가 금액 넣기", "2. 메뉴 다시 고르기", "3. 그만 두기"]
         self.quesDict = self.CreateDict() # { 1 : "추가 금액 넣기"...
-        self.quesNoneNumDict = self.CreateNoneNumDict() # ["추가금액넣기"...
+        #self.quesNoneNumDict = self.CreateNoneNumDict() # ["추가금액넣기"...
         self.quesReversDict = self.CreateReversDict() # ["추가금액넣기" : 1...
         # 주의점 이거 키 넣을때 입력값에 replace() 해줘야 함
     def CreateDict(self):
@@ -13,16 +13,16 @@ class Question:
             QuestionDict[dict_Key] = dict_Value
         return QuestionDict
 
-    def CreateNoneNumDict(self):
-        QuestionNoneNumList = {}
-        # key와 Value 둘다 얻기 위해서는 딕셔너리.items()를 해줘야하고
-        # key만 얻기 위해서는 딕셔너리만 써도된다.
-        for key,value in self.quesDict.items():
-            # strip 이건 양쪽 끝만 공백을 제거하고
-            # replace 이건 전부다 제거 할 수도 있고 3번째 인수에는 횟수를 지정할 수도 있다.
-            value = value.replace(" ", "")
-            QuestionNoneNumList[value] = value
-        return QuestionNoneNumList
+    # def CreateNoneNumDict(self):
+    #     QuestionNoneNumList = {}
+    #     # key와 Value 둘다 얻기 위해서는 딕셔너리.items()를 해줘야하고
+    #     # key만 얻기 위해서는 딕셔너리만 써도된다.
+    #     for key,value in self.quesDict.items():
+    #         # strip 이건 양쪽 끝만 공백을 제거하고
+    #         # replace 이건 전부다 제거 할 수도 있고 3번째 인수에는 횟수를 지정할 수도 있다.
+    #         value = value.replace(" ", "")
+    #         QuestionNoneNumList[value] = value
+    #     return QuestionNoneNumList
 
     def CreateReversDict(self):
         QuestionReversDict = {}
