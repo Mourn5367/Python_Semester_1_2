90adf6c6134bdbfa22f198afb01c361f301479c8
Mourn5367 <qwerrrt6@gmail.com>
2024-10-02 00:00:04 +0900

    10으로 해버리면 10 초과 미만일때 자리가 문제가 발생함. cols 값으로 하는게 맞았음. 함수로 바꾸고 예외처리 하는중

--- SeatLottery/tmpMain.py
+++ SeatLottery/tmpMain.py
@@ -1,37 +1,68 @@
 import random
 import copy
+import sys
+
 from Group import Group
+
+def superpeople(superlist : list, gr: Group) -> list:
+    reply = input("선택권 있나요 (있는 경우 ""네 입력"") ")
+
+
+    if reply.replace(" ","") == "네":
+        try:
+            count = int(input("몇자리 까지 선택 하겠습니까? (숫자로 입력)"))
+        except ValueError:
+            print("숫자 이외의 입력입니다. 좌석 선택 함수를 종료합니다.")
+            return superlist
+        if count > gr.totalSeat:
+            print("현재 좌석보다 많습니다. 좌석 선택 함수를 종료합니다.")
+            return superlist
+        for i in range(count):
+            name = input(f"{i + 1}번째 사람의 이름은?")
+            superList[0].append(name)
+            try:
+                position = int(input(f"{name}의 자리는? (ex 첫(01)번째 자리는 ""1""만 입력해 주세요."))
+                superList[1].append(position)
+            except ValueError:
+                print("숫자 이외의 입력입니다. 좌석 선택 함수를 종료합니다.")
+        superList[1].sort()
+    return superlist
+
 if __name__ == '__main__':
-    # seatList = []
+
+    step = 0
     gr = Group()
-    gr.cols_count()
-    gr.rows_count()
-    # totalStudents = 28 # 총 학생 수
-    # for i in range(rows):
-    #     seatList.append([])
-    #     for j in range(cols):
-    #         seatList[i].append(j+1+i*10)
-    # seatList = [[j+1+i*10 for j in range(cols)] for i in range(rows)]
-    # print(seatList)
+
+    try:
+        gr.cols_count()
+        gr.rows_count()
+    except ValueError:
+        print("유효 하지 않는 입력입니다.")
+        sys.exit()
 
     superList = [[],[]]
     print(len(superList[0]))
-    reply = input("선택권 있나요")
-    if reply == "네":
-        count = int(input("몇자리 까지?"))
-        for i in range(count):
-            name = str(input(f"{i+1}번째 사람의 이름은?"))
-            superList[0].append(name)
-            position = int(input(f"{name}의 자리는?"))
-            superList[1].append(position)
-        superList[1].sort()
-
+    superList = superpeople(superList,gr)
+    # reply = input("선택권 있나요")
+    # if reply == "네":
+    #     count = int(input("몇자리 까지?"))
+    #     for i in range(count):
+    #         name = str(input(f"{i+1}번째 사람의 이름은?"))
+    #         superList[0].append(name)
+    #         position = int(input(f"{name}의 자리는?"))
+    #         superList[1].append(position)
+    #     superList[1].sort()
 
     # nameList = input("이름을 입력하세요.(띄어 쓰기로 구분)").split(" ")
     nameList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] # 학생 이름 적기
     if len(nameList) < gr.totalSeat:
         for i in range(gr.totalSeat - (len(nameList) + len(superList[0]))):
             nameList.append(f"EMPTY")
+
+    if len(nameList) > gr.totalSeat:
+        print("좌석 수 보다 인원 수 가 더 많습니다. 프로그램을 종료합니다.")
+        sys.exit()
+
     copyNameList = copy.deepcopy(nameList)
     seatL = []
 
@@ -39,18 +70,18 @@ if __name__ == '__main__':
         seatL.append({})
         for j in range(gr.cols):
             random.shuffle(copyNameList)
-            if len(superList[1]) >= 1 and (j+i*10+1) == superList[1][0]:
-                seatL[i][j+i*10+1] = superList[0].pop(0)
+            if len(superList[1]) >= 1 and (j+i*gr.cols+1) == superList[1][0]:
+                seatL[i][j+i*gr.cols+1] = superList[0].pop(0)
                 superList[1].pop(0)
             else:
-                seatL[i][j+i*10+1] = copyNameList.pop()
+                seatL[i][j+i*gr.cols+1] = copyNameList.pop()
     # nameDict = {}
     # for i in range(1,totalSeat+1):
     #     nameDict[i] = nameList[i-1]
     # print(seatL)
     for i in range(gr.rows):
         for j in range(gr.cols, 0, -1): # (1,cols+1)
-            print(f"{i*10+j:02}번 {seatL[i][j+i*10]:<3}\t",end="")
+            print(f"{i*gr.cols+j:02}번 {seatL[i][j+i*gr.cols]:<3}\t",end="")
         print("\n")
     # print(seatL[0])
     # print(seatL[1])
